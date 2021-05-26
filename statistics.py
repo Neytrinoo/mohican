745bc8fa4e57c14737662f7d130a197e96d9ea5b
kirill555101 <kir20-01@ya.ru>
2021-05-27 00:33:36 +0300

    Some changes in file

--- statistics.py
+++ statistics.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
 
+import subprocess
 import matplotlib.pyplot as plt
 
 paths = [
     './mohican.log',
 ]
 
+def autolabel(rects, labels=None, height_factor=1.01):
+    for i, rect in enumerate(rects):
+        height = rect.get_height()
+        if labels is not None:
+            try:
+                label = labels[i]
+            except (TypeError, KeyError):
+                label = ' '
+        else:
+            label = '%d' % int(height)
+        ax.text(rect.get_x() + rect.get_width()/2., height_factor*height,
+                '{}'.format(label),
+                ha='center', va='bottom')
+
 names = []
 values = []
 
 for path in paths:
-    number = 0
-    with open(path, 'r') as file:
-        while True:
-            line = file.readline()
+    number = int(subprocess.check_output('cat {} | grep "New connection" | wc -l'.format(path), shell=True))
 
-            if not line:
-                break
-
-            if line.find('New connection') != -1:
-                number += 1
-    
-    names.append(str(path.index(path) + 1))
+    names.append('№' + str(path.index(path) + 1))
     values.append(number)
 
 fig,ax = plt.subplots()
@@ -35,5 +41,7 @@ ax.set_ylabel('number of requests')
 fig.set_facecolor('floralwhite')
 fig.set_figwidth(8)
 fig.set_figheight(6)
+
+autolabel(ax.patches, values, height_factor=1)
 
 plt.show()
